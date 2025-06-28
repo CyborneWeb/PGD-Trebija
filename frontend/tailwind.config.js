925f76b7ab3dbc8f12611e4f56b819e98731bce5
Matic Štucin <matic.stucin@gmail.com>
2025-06-28 10:51:20 +0200

    Add custom 'nav' screen breakpoint to Tailwind config
    Introduced a new 'nav' screen size at 1060px in the Tailwind CSS configuration to support responsive design for navigation components.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "media", // or 'class' if you want manual toggle
   theme: {
-    extend: {},
+    extend: {
+        screens: {
+            "nav" : "1060px"
+        }
+    },
   },
   plugins: [],
 };
