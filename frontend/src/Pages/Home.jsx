62af8e1d8757bcc16e47ee8a7d9a584e1a24aa04
Matic Štucin <matic.stucin@gmail.com>
2025-06-29 16:37:19 +0200

    Add NotifContainer and Donate to Home page
    Imported and rendered NotifContainer and Donate components in the Home page to enhance functionality and user engagement.

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero";
+import NotifContainer from "../components/NotifContainer";
+import Donate from "../components/Donate";
 
 
 const Home = () => {
   return (
     <>
     <Hero></Hero>
+    <NotifContainer />
+    <Donate />
+
     </>
 
   );
