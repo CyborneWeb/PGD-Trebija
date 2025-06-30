dcdc3f52775f1c057fee397979f478a454fdb21a
Matic Štucin <matic.stucin@gmail.com>
2025-06-30 12:08:09 +0200

    Add WeatherContainer to Home page
    Introduced the WeatherContainer component to the Home page, grouping it with NotifContainer inside a div for improved layout and information display.

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import Hero from "../components/Hero";
 import NotifContainer from "../components/NotifContainer";
 import Donate from "../components/Donate";
-
+import WeatherContainer from "../components/Containers/WeatherContainer";
 
 const Home = () => {
   return (
     <>
-    <Hero></Hero>
-    <NotifContainer />
-    <Donate />
-
+      <Hero />
+      <div className="">
+        <WeatherContainer />
+        <NotifContainer />
+        
+      </div>
+      <Donate />
     </>
-
   );
 };
 
