565b4a32c7d5a067a4f3217f6f5607ab568b6133
Matic Štucin <matic.stucin@gmail.com>
2025-07-01 13:31:18 +0200

    Update tailwind.config.js
    Changed darkMode toggling to class

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "media", // or 'class' if you want manual toggle
+  darkMode: "class", // or 'class' if you want manual toggle
   theme: {
     extend: {
         screens: {
