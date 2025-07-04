51ad582e38eac365159b7bf4db5da19c7ca29f73
Matic Štucin <matic.stucin@gmail.com>
2025-07-04 18:53:07 +0200

    Add Toast notifications and ScrollToTop to App
    Integrated react-toastify's ToastContainer for notifications, using the current theme from ThemeContext. Added ScrollToTop component to handle scroll behavior on route changes.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,31 +6,50 @@ import Home from "./Pages/Home";
 import BackToTop from "./components/Buttons/BackToTop";
 import CookieModal from "./components/Modals/CookieModal";
 import Contact from "./Pages/Contact";
+import ScrollToTop from "./components/Other/ScrollToTop";
+import {ToastContainer} from "react-toastify";
+import { useTheme } from "./Contexts/ThemeContext";
 
 
 function App() {
+  const { theme } = useTheme();
  
 
   return (
-    <Router>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/drustvo" element={<About />} />
-            <Route path="/obvestila" element={<Posts />} />
-            <Route path="/galerija" element={<Gallery />} />
-            <Route path="/kontakt" element={<Contact />} />
-          </Routes>
-        </main>
-        <BackToTop />
-        <CookieModal />
-
-        <Footer />
-      </div>
-    </Router>
+    <div>
+      
+      <Router>
+        <ScrollToTop />
+        <div className="min-h-screen flex flex-col">
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme={theme}
+          />
+          
+          <Navbar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/drustvo" element={<About />} />
+              <Route path="/obvestila" element={<Posts />} />
+              <Route path="/galerija" element={<Gallery />} />
+              <Route path="/kontakt" element={<Contact />} />
+            </Routes>
+          </main>
+          <BackToTop />
+          <CookieModal />
+          <Footer />
+        </div>
+      </Router>
+    </div>
   );
 }
 
