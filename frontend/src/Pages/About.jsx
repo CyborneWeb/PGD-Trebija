a637d338c70bfec133fe9c4caa6ab30e2747f1fc
Matic Štucin <matic.stucin@gmail.com>
2025-07-05 15:21:10 +0200

    Add About page component
    Introduces the About.jsx page, displaying a header with title and subtitle about the organization.

---
+++ frontend/src/Pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../components/Headers/PageHeader'
+
+const About = () => {
+  return (
+    <PageHeader
+      title="O nas"
+      subtitle="Spoznajte našo zgodovino ter izvedite več o našem društvu"
+    />
+    
+  )
+}
+
+export default About
